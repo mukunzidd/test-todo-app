884fcd427470824ed4c1eddbc874fcfd2742c2e8
Dieudonne Mukunzi <dmukunzi0@gmail.com>
2017-03-07 08:42:24 +0200

    Added a test for incrementing task priority

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
+
+  # Task toggle_favorite test
   describe '#toggle_complete!' do 
     it 'should switch complete to false if it began as true' do
       task = Task.new(complete: true)
@@ -13,6 +15,8 @@ RSpec.describe Task, type: :model do
       task.toggle_complete!
       expect(task.complete).to eq(true)
     end
+
+  # Task toggle_favorite test
   describe '#toggle_favorite!'
     it 'should switch favorite to false if it began as true' do
       task = Task.new(favorite: true)
@@ -27,6 +31,7 @@ RSpec.describe Task, type: :model do
     end
   end
 
+  # Task overdue? test
   describe '#overdue?' do
     it 'should return true if Time.now > deadline!' do
       task = Task.new(deadline: Time.now - 2.hours)
@@ -36,6 +41,15 @@ RSpec.describe Task, type: :model do
     it 'should return true if Time.now > deadline!' do
       task = Task.new(deadline: Time.now + 2.hours)
       expect(task.overdue?).to eq(false)
+    end
+  end
+
+  # Task decrement priority!
+  describe '#increment_priority' do
+    it 'should return increase priority by 1 if priority is less than ten' do
+      task = Task.new(priority: 5)
+      task.increment_priority!
+      expect(task.priority).to eq(6)
     end
   end
 end
