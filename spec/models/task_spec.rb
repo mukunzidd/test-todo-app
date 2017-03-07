d07a6ff06a4215601c9765c44ce2c3680f42a454
Dieudonne Mukunzi <dmukunzi0@gmail.com>
2017-03-07 08:22:14 +0200

    Added a test for toogle_favorite method

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,16 +2,28 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   describe '#toggle_complete!' do 
-    it "should switch complete to false if it began as true" do
+    it 'should switch complete to false if it began as true' do
       task = Task.new(complete: true)
       task.toggle_complete!
       expect(task.complete).to eq(false)
     end
 
-    it "should switch complete to true if it began as false" do 
+    it 'should switch complete to true if it began as false' do 
       task = Task.new(complete: false)
       task.toggle_complete!
       expect(task.complete).to eq(true)
+    end
+  describe '#toggle_favorite!'
+    it 'should switch favorite to false if it began as true' do
+      task = Task.new(favorite: true)
+      task.toggle_favorite!
+      expect(task.favorite).to eq(false)
+    end
+
+    it 'should switch favorite to true if it began as false' do
+      task = Task.new(favorite: false)
+      task.toggle_favorite!
+      expect(task.favorite).to eq(true)
     end
   end
 end
